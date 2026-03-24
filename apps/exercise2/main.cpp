be2d15b609a54b4942127c6f8ba5bca7a59e3ffc
DogGuyMan <escatrgot@gmail.com>
2026-03-24 17:36:39 +0900

    [dev] : 과제끝

--- apps/exercise2/main.cpp
+++ apps/exercise2/main.cpp
@@ -102,8 +102,7 @@ namespace exercise2
 		static constexpr const GLchar *vs = R"(
 			#version 410 core
 			layout (location = 0) in float currentTime; 
-			// [1] 
-			layout(location = 0) in vec4 vertexColor;
+			// [1] layout(location = 0) in vec4 vertexColor;
 
 			out VS_OUT {
 				vec4 color;
@@ -128,10 +127,13 @@ namespace exercise2
 				float curTimeCos = cos(currentTime) * 0.5 + 0.5f;
 				float curTimeSin = sin(currentTime) * 0.5 + 0.5f;
 				vec4 vertexTints[3] = vec4[3](
-					vec4(curTimeCos, curTimeSin, curTimeCos, 1.0),
-					vec4(curTimeSin, curTimeCos, curTimeSin, 1.0),
-					vec4(curTimeSin, curTimeSin, curTimeCos, 1.0)
+					vec4(1, 0, 0, 1.0),
+					vec4(0, 1, 0, 1.0),
+					vec4(0, 0, 1, 1.0)
 				);
+				vertexTints[0] = (vertexTints[0] * 0.5 + curTimeCos * 0.5);
+				vertexTints[1] = (vertexTints[1] * 0.5 + curTimeSin * 0.5);
+				vertexTints[2] = (vertexTints[2] * 0.5 + curTimeSin * 0.5);
 
 				int bladeID = gl_VertexID / 3;
 				int bladeVertID = gl_VertexID % 3;
@@ -143,12 +145,19 @@ namespace exercise2
 					curRotateCos, curRotateSin,
 					-curRotateSin, curRotateCos
 				);
+				mat4 mov = mat4(
+					1.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, 0.0, 0.0,
+					0.0, 0.0, 1.0, 0.0,
+					cos(currentTime) * 0.5, 0.0, 0.0, 1.0
+				);
 				vec2 rotated = rot * base[bladeVertID].xy;
-				gl_Position = vec4(rotated, 0.0, 1.0);
-				// [1] 
-				vs_out.color = vertexColor;
+				vec4 moved = mov * vec4(rotated.xy, 0.0, 1.0);
+				gl_Position = moved;
+				// [1] vs_out.color = vertexColor;
 				// [2] vs_out.color = bladeColors[bladeID];
-				// [3] vs_out.color = vec4(vertexTints[bladeVertID].rgb, 1.0);
+				// [3]
+				vs_out.color = vec4(vertexTints[bladeVertID].rgb, 1.0);
 			}
 		)";
 		static constexpr const GLchar *fs = R"(
@@ -168,15 +177,59 @@ namespace exercise2
 						   {GL_FRAGMENT_SHADER, fs}}) {}
 		
 		virtual void Draw(float currentTime) const override { 
-			GLfloat bladeColors[] = {
-				1.0, 0.0, 0.0, 1.0,  // blade 0: 빨강
-				0.0, 1.0, 0.0, 1.0,  // blade 1: 초록
-				0.0, 0.0, 1.0, 1.0,  // blade 2: 파랑
-				1.0, 1.0, 0.0, 1.0   // blade 3: 노랑
-			};
-			glVertexAttrib4fv(1, bladeColors);
-			// glVertexAttrib1f(0, (float)currentTime);
+			glVertexAttrib1f(0, (float)currentTime);
 			glDrawArrays(GL_TRIANGLES, 0, 12);
+		}
+	};
+
+	class stick_program : public program_base
+	{
+		static constexpr const GLchar *vs = R"(
+			#version 410 core
+			layout (location = 0) in float currentTime; 
+
+			void main(void) {
+				const vec4 base[6] = vec4[6](
+					vec4(0.01, 0.0, 0.0, 1.0),
+					vec4(-0.01, 0.0, 0.0, 1.0),
+					vec4(-0.01, -100.0, 0.0, 1.0),
+					vec4(0.01, 0.0, 0.0, 1.0),
+					vec4(-0.01, -100.0, 0.0, 1.0),
+					vec4(-0.01, -100.0, 0.0, 1.0)
+				);
+				
+				int bladeID = gl_VertexID / 3;
+				int bladeVertID = gl_VertexID % 3;
+				
+				mat4 mov = mat4(
+					1.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, 0.0, 0.0,
+					0.0, 0.0, 1.0, 0.0,
+					cos(currentTime) * 0.5, 0.0, 0.0, 1.0
+				);
+				vec4 moved = mov * base[bladeVertID];
+				gl_Position = moved;
+			}
+		)";
+		static constexpr const GLchar *fs = R"(
+			#version 410 core
+			out vec4 color;
+
+			in VS_OUT {
+				vec4 color;
+			} fs_in;
+			void main(void) {
+				color = vec4(1.0, 0.0, 1.0, 1.0);
+			}
+		)";
+
+	public:
+		stick_program() : program_base({{GL_VERTEX_SHADER, vs},
+						   {GL_FRAGMENT_SHADER, fs}}) {}
+		
+		virtual void Draw(float currentTime) const override { 
+			glVertexAttrib1f(0, (float)currentTime);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
 		}
 	};
 
@@ -184,12 +237,13 @@ namespace exercise2
 	{
 	private:
 		std::vector<std::unique_ptr<program_base>> programs;
-		GLfloat background_color[4] = {0.5f, 0.5f, 0.5f, 1.0f};
+		GLfloat background_color[4] = {0.0f,0.0f,0.0f, 1.0f};
 
 	public:
 		virtual void startup() override
 		{
 			// programs = std::vector<program_base>(2);
+			programs.push_back(std::make_unique<stick_program>());
 			programs.push_back(std::make_unique<pinwheel_program>());
 		}
 
